1939831bffe6639b779a7d8d7b33e3bb435034ca
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2024-06-25 23:18:54 -0300

    commit correct

---
+++ README.md
@@ -0,0 +1,14 @@
+#APi - é um lugar para disponibilar recursos ou funcionalidades
+
+# 1. Objetivo - Criar um api de disponibiliza a consulta, criação, edição e exclusão de livros 
+
+# 2. URL base - localHost
+
+# 3. EndPoints - 
+    # - localHost/livros (GET)
+    # - localHost/livros (POST)
+    # - localHost/livros/id (GET)
+    # - localHost/livros/id (PUT)
+    # - localHost/livros/id (DELETE)
+
+# 4. Quais recursos - Livros
--- app.py
+++ app.py
@@ -1,17 +1,3 @@
-#APi - é um lugar para disponibilar recursos ou funcionalidades
-
-# 1. Objetivo - Criar um api de disponibiliza a consulta, criação, edição e exclusão de livros 
-
-# 2. URL base - localHost
-
-# 3. EndPoints - 
-    # - localHost/livros (GET)
-    # - localHost/livros (POST)
-    # - localHost/livros/id (GET)
-    # - localHost/livros/id (PUT)
-    # - localHost/livros/id (DELETE)
-
-# 4. Quais recursos - Livros
 
 from flask import Flask, jsonify, request
 
